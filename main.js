30d86db635e063d4fd92d53496b91f82953cbf51
Naima <naima.lalmi28@gmail.com>
2024-10-03 15:00:00 +0200

    map avec layer

--- main.js
+++ main.js
@@ -55,14 +55,13 @@ var bounds = [
 // --- MAP LIMITS ---
 map.setMaxBounds(bounds)
 // --- WATER COLOR LAYER ONTO THE MAP ---
-// var Stadia_StamenWatercolor = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}',
-//   {
-//     minZoom: 5,
-//     maxZoom: 7,
-//     attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//     ext: 'jpg'
-//   });
-// Stadia_StamenWatercolor.addTo(map);
+var Stadia_StamenWatercolor = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    minZoom: 5,
+    maxZoom: 7,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  });
+Stadia_StamenWatercolor.addTo(map);
 // --- MAP ZOOM CONTROL POSITION ---
 map.zoomControl.setPosition('bottomright')
 // --- CITIES MARKERS ---
